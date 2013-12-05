77f7cb7d28ffd27b10944888a46ade56778d6fcf
Han-Yu,Wang <huw12313212@gmail.com>
2013-12-05 21:44:34 +0800

    Synchrounous Test

--- Kinect/Assets/Toon Character Pack/Scripts/BotControlScript.cs
+++ Kinect/Assets/Toon Character Pack/Scripts/BotControlScript.cs
@@ -10,6 +10,75 @@ using System.Runtime.InteropServices;
 
 public class BotControlScript : MonoBehaviour
 {
+	
+	 private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
+
+    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        Vector3 syncPosition = Vector3.zero;
+        Vector3 syncVelocity = Vector3.zero;
+        if (stream.isWriting)
+        {
+            syncPosition = rigidbody.position;
+            stream.Serialize(ref syncPosition);
+
+            syncPosition = rigidbody.velocity;
+            stream.Serialize(ref syncVelocity);
+        }
+        else
+        {
+            stream.Serialize(ref syncPosition);
+            stream.Serialize(ref syncVelocity);
+
+            syncTime = 0f;
+            syncDelay = Time.time - lastSynchronizationTime;
+            lastSynchronizationTime = Time.time;
+
+            syncEndPosition = syncPosition + syncVelocity * syncDelay;
+            syncStartPosition = rigidbody.position;
+        }
+    }
+
+    void Awake()
+    {
+        lastSynchronizationTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (networkView.isMine)
+        {
+            InputMovement();
+        }
+        else
+        {
+            SyncedMovement();
+        }
+    }
+
+
+    private void InputMovement()
+    {
+        
+    }
+
+    private void SyncedMovement()
+    {
+        syncTime += Time.deltaTime;
+
+        rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+    }
+
+
+
+	
+	
+	
+	
 	int moveSpeed = 5;
 	
 	[DllImport ("UniWii")]
