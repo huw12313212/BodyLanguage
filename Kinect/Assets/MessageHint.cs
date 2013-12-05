4c439bc3e03249b5e5674d0012cf594328781b0f
11sdfjsakl <hcy810125@yahoo.com.tw>
2013-12-05 19:25:16 +0800

    Message UI

---
+++ Kinect/Assets/MessageHint.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MessageHint : MonoBehaviour {
+	
+	public List<int> message = new List<int>();
+	public List<UILabel> Labels = new List<UILabel>();
+	
+	// Use this for initialization
+	void Start () {
+	
+		Labels[0].text = message[0] + "";
+				Labels[1].text = message[1] + "";
+				Labels[2].text = message[2] + "";
+				Labels[3].text = message[3] + "";
+		
+		
+		
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
