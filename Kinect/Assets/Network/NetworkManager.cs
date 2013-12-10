55980c5fcf979ac36481909b82f8d60dc90f20eb
tony056 <tony61507@gmail.com>
2013-12-10 21:05:31 +0800

    network ui change to non-gui

--- Kinect/Assets/Network/NetworkManager.cs
+++ Kinect/Assets/Network/NetworkManager.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class NetworkManager : MonoBehaviour
 {
-    private const string typeName = "UniqueGameName";
-    private const string gameName = "RoomName";
-
-    private bool isRefreshingHostList = false;
-    private HostData[] hostList;
-
-    public GameObject playerPrefab;
-
-    void OnGUI()
+	private const string typeName = "BodyLanguageBL";
+	private const string gameName = "RoomName24";
+	
+	private bool isRefreshingHostList = false;
+	private HostData[] hostList;
+	private float startTime;
+	public GameObject playerPrefab;
+	bool flagServer = false;
+	bool flagClient = false;
+	
+	/*void OnGUI()
     {
         if (!Network.isClient && !Network.isServer)
         {
@@ -30,53 +32,102 @@ public class NetworkManager : MonoBehaviour
                 }
             }
         }
-    }
-
-    private void StartServer()
-    {
-        Network.InitializeServer(5, 25000, !Network.HavePublicAddress());
-        MasterServer.RegisterHost(typeName, gameName);
-    }
-
-    void OnServerInitialized()
-    {
-        SpawnPlayer();
-    }
-
-
-    void Update()
-    {
-        if (isRefreshingHostList && MasterServer.PollHostList().Length > 0)
-        {
-            isRefreshingHostList = false;
-            hostList = MasterServer.PollHostList();
-        }
-    }
-
-    private void RefreshHostList()
-    {
-        if (!isRefreshingHostList)
-        {
-            isRefreshingHostList = true;
-            MasterServer.RequestHostList(typeName);
-        }
-    }
-
-
-    private void JoinServer(HostData hostData)
-    {
-        Network.Connect(hostData);
-    }
-
-    void OnConnectedToServer()
-    {
-        SpawnPlayer();
-    }
-
-
-    private void SpawnPlayer()
-    {
-        GameObject player = (GameObject)Network.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity, 0);
-
-    }
+    }*/
+	void Start(){
+		startTime = Time.time;
+	}
+	private void StartServer()
+	{
+		Network.InitializeServer(5, 25002, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	
+	void OnServerInitialized()
+	{
+		SpawnPlayer();
+		Debug.Log ("Initialized");
+	}
+	
+	
+	void Update()
+	{
+		
+		//if (isRefreshingHostList && MasterServer.PollHostList().Length > 0)
+		//{
+		RefreshHostList();
+		hostList = MasterServer.PollHostList();
+		
+		
+		//check the room name of the game
+		/*if (hostList.Length > 0) {
+			string name = "";
+			for(int i = 0;i < hostList.Length;i++){
+				name += hostList[i].gameName;
+				name += " ";
+			}
+			Debug.Log("NAME: " + name);
+		}*/
+		
+		//check wether the server is built or not 
+		if (flagServer == false && flagClient == false) {
+			if (Time.time - startTime > 3.0f) {
+				Debug.Log("update: "+hostList.Length);
+				Debug.Log("passed time gap!?qqqqq");
+				if (hostList.Length == 0) {
+					StartServer ();
+					flagServer = true;
+				} 
+				else {
+					Debug.Log ("I am the client,,,,,,,");
+					for (int i = 0; i < hostList.Length; i++) {
+						if (hostList [i].gameName == gameName) {
+							JoinServer (hostList[i]);
+							flagClient = true;
+							break;
+						}
+					}
+				}
+			}
+		}
+		//isRefreshingHostList = false;
+		
+		
+		
+	}
+	
+	private void RefreshHostList()
+	{
+		//Debug.Log ("RefreshHostList");
+		if (!isRefreshingHostList)
+		{
+			//isRefreshingHostList = true;
+			//MasterServer.RequestHostList(typeName);
+			
+			MasterServer.RequestHostList(typeName);
+			hostList = MasterServer.PollHostList();
+			//Debug.Log("hostList: "+hostList);
+			
+		}
+	}
+	
+	
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+		Debug.Log ("I connect to the server: " + hostData.gameName);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log ("connected to server QQ");
+		SpawnPlayer();
+		
+	}
+	
+	
+	private void SpawnPlayer()
+	{
+		GameObject player = (GameObject)Network.Instantiate(playerPrefab, Vector3.zero, Quaternion.identity, 0);
+		
+	}
 }
