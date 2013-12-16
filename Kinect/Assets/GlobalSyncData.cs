af8b045d34b75ce08ea5492efdaed3d9f9e36007
tony056 <tony61507@gmail.com>
2013-12-17 05:04:17 +0800

    sync turntables in the puzzle 2

--- Kinect/Assets/GlobalSyncData.cs
+++ Kinect/Assets/GlobalSyncData.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class GlobalSyncData : MonoBehaviour {
-
+	
 	public int puzzle1AnswerSize;
 	public int puzzle2AnswerSize;
 	public int puzzle3AnswerSize;
@@ -11,7 +11,11 @@ public class GlobalSyncData : MonoBehaviour {
 	public List<int> puzzle2Answer;
 	public List<string> puzzle3Answer;
 	public List<string> puzzle3AnswerStringSet;
-
+	
+	public GameObject puzzle2Turntable1;
+	public GameObject puzzle2Turntable2;
+	public GameObject puzzle2Turntable3;
+	
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Start!");
@@ -19,12 +23,16 @@ public class GlobalSyncData : MonoBehaviour {
 		puzzle1AnswerSize = 3;
 		puzzle2AnswerSize = 3;
 		puzzle3AnswerSize = 1;
-
+		
 		//init
 		puzzle1Answer = new List<int>();
 		puzzle2Answer = new List<int>();
 		puzzle3Answer = new List<string>();
-
+		
+		puzzle2Turntable1 = GameObject.Find ("Puzzle 2 Sensor 1");
+		puzzle2Turntable2 = GameObject.Find ("Puzzle 2 Sensor 2");
+		puzzle2Turntable3 = GameObject.Find ("Puzzle 2 Sensor 3");
+		
 		//init puzzle answer
 		if(networkView.isMine)
 		{
@@ -37,19 +45,19 @@ public class GlobalSyncData : MonoBehaviour {
 				//random answer
 				randomAnswer.Add(Random.Range(0,puzzle1AnswerSize));
 			}
-
+			
 			//RPC sync data
 			networkView.RPC("setPuzzle1RPC",RPCMode.AllBuffered,randomAnswer[0],randomAnswer[1],randomAnswer[2]);
-
+			
 			//puzzle 2
-
+			
 			//zero flag
 			bool zeroFlag = true;
 			//check all zero or not
 			while(zeroFlag){
 				//clear  random answer
 				randomAnswer.Clear();
-
+				
 				//create random answer
 				for(int i = 0;i<puzzle2AnswerSize;i++)
 				{
@@ -59,15 +67,15 @@ public class GlobalSyncData : MonoBehaviour {
 					if(randomNum!=0) zeroFlag = false;
 				}
 			}
-
+			
 			//RPC sync data
 			networkView.RPC("setPuzzle2RPC",RPCMode.AllBuffered,randomAnswer[0],randomAnswer[1],randomAnswer[2]);
-
+			
 			//puzzle 3
-
+			
 			//clear  random answer
 			randomAnswer.Clear();
-
+			
 			//create random answer
 			for(int i = 0;i<puzzle3AnswerSize;i++)
 			{
@@ -75,19 +83,38 @@ public class GlobalSyncData : MonoBehaviour {
 				int randomNum = Random.Range(0,3);
 				randomAnswer.Add(randomNum);
 			}
-
+			
 			//RPC sync data
 			networkView.RPC("setPuzzle3RPC",RPCMode.AllBuffered,randomAnswer[0]);
-
+			
 		}
-
+		
 	}
-
+	
 	void Update()
 	{
+		
+		if (puzzle2Turntable1 != null) {
+			Puzzle2PanelSensor1 turnTable1 = puzzle2Turntable1.GetComponent<Puzzle2PanelSensor1>();
+			if(turnTable1.isTrigger && turnTable1.ButtonB){
+				networkView.RPC("updatePanelRotation",RPCMode.AllBuffered,1, turnTable1.addRotation, turnTable1.clickCount, turnTable1.initialRotation); 
+			}
+		}
+		if (puzzle2Turntable2 != null) {
+			Puzzle2PanelSensor1 turnTable2 = puzzle2Turntable2.GetComponent<Puzzle2PanelSensor1>();
+			if(turnTable2.isTrigger && turnTable2.ButtonB){
+				networkView.RPC("updatePanelRotation",RPCMode.AllBuffered,2, turnTable2.addRotation, turnTable2.clickCount, turnTable2.initialRotation); 
+			}
+		}
+		if (puzzle2Turntable3 != null) {
+			Puzzle2PanelSensor1 turnTable3 = puzzle2Turntable3.GetComponent<Puzzle2PanelSensor1>();
+			if(turnTable3.isTrigger && turnTable3.ButtonB){
+				networkView.RPC("updatePanelRotation",RPCMode.AllBuffered,3, turnTable3.addRotation, turnTable3.clickCount, turnTable3.initialRotation); 
+			}
+		}
 		//update
 	}
-
+	
 	void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
 	{
 		List<int> syncTemp;
@@ -96,7 +123,7 @@ public class GlobalSyncData : MonoBehaviour {
 		{
 			Debug.Log("Wirting!");
 			//syncTemp = puzzle1Answer;
-
+			
 			/*
 			for(int i =0;i<syncTemp.Count;i++)
 			{
@@ -109,7 +136,7 @@ public class GlobalSyncData : MonoBehaviour {
 		{
 			Debug.Log("Received!");
 			//stream.Serialize(ref syncTemp);
-
+			
 			/*
 			for(int i =0;i<syncTemp.Count;i++)
 			{
@@ -119,23 +146,24 @@ public class GlobalSyncData : MonoBehaviour {
 			//syncTemp = puzzle1Answer;
 		}
 	}
-
+	
 	public void setPuzzle1Answer(List<int> answer)
 	{
 		if(answer!=null) puzzle1Answer = answer;
 	}
-
+	
+	
 	[RPC]
 	void setPuzzle1RPC(int i1,int i2,int i3)
 	{
 		//clear
 		puzzle1Answer.Clear();
-
+		
 		puzzle1Answer.Add(i1);
 		puzzle1Answer.Add(i2);
 		puzzle1Answer.Add(i3);
 	}
-
+	
 	[RPC]
 	void setPuzzle2RPC(int i1,int i2,int i3)
 	{
@@ -146,7 +174,7 @@ public class GlobalSyncData : MonoBehaviour {
 		puzzle2Answer.Add(i2);
 		puzzle2Answer.Add(i3);
 	}
-
+	
 	[RPC]
 	void setPuzzle3RPC(int i1)
 	{
@@ -154,5 +182,30 @@ public class GlobalSyncData : MonoBehaviour {
 		puzzle3Answer.Clear();
 		
 		puzzle3Answer.Add(puzzle3AnswerStringSet[i1]);
+	}
+	
+	[RPC] 
+	void updatePanelRotation(int order, float addRotation, int clickCount, Vector3 initialRotation){
+		TweenRotation tween = null;
+		float rotationOffset = 0;
+		if (order == 1) {
+			tween = puzzle2Turntable1.GetComponent<TweenRotation>();
+			rotationOffset = (addRotation*clickCount)%360;
+			tween.from = puzzle2Turntable1.transform.rotation.eulerAngles;
+		} else if (order == 2) {
+			tween = puzzle2Turntable2.GetComponent<TweenRotation>();
+			rotationOffset = (addRotation*clickCount)%360;
+			tween.from = puzzle2Turntable2.transform.rotation.eulerAngles;
+		} else {
+			tween = puzzle2Turntable3.GetComponent<TweenRotation>();
+			rotationOffset = (addRotation*clickCount)%360;
+			tween.from = puzzle2Turntable3.transform.rotation.eulerAngles;		
+		}
+		
+		if (tween != null) {
+			tween.to = initialRotation + (new Vector3(0,0,rotationOffset));
+			tween.Reset();
+			tween.enabled = true;
+		}
 	}
 }
--- Kinect/Assets/Puzzle2PanelSensor1.cs
+++ Kinect/Assets/Puzzle2PanelSensor1.cs
@@ -2,45 +2,58 @@
 using System.Collections;
 
 public class Puzzle2PanelSensor1 : MonoBehaviour {
-
+	
 	public TweenRotation tween;
-	private Vector3 initialRotation;
+	public Vector3 initialRotation;
 	public int myIndex;
 	public Puzzle2Manager manager;
 	public int partationNum;
-	private bool isTrigger;
-	private int clickCount = 0;
-	private bool ButtonB = false;
-	private float addRotation;
-
+	public bool isTrigger;
+	public int clickCount = 0;
+	public bool ButtonB = false;
+	public float addRotation;
+	public int order = -1;
+	public GameObject globalSync = null;
 	// Use this for initializationth
 	void Start () {
 		//initial position
 		initialRotation = gameObject.transform.rotation.eulerAngles;
 		isTrigger = false;
 		addRotation = 360/partationNum;
-
+		globalSync = GameObject.Find ("GlobalSyncData");
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		if (globalSync == null) {
+			globalSync = GameObject.Find ("GlobalSyncData");
+		} else
+			Debug.Log ("nul!!");
+		
 		if (isTrigger){	
-
+			
 			//get button down
 			ButtonB =  Input.GetButtonDown("ButtonB");
 			//click button b
-
+			
 			if (ButtonB){
 				//click count
 				clickCount ++;
-
+				
 				//rotate game object
+				detectNumber();
+				if(order != -1){
+					
+					order = -1;
+				}else
+					Debug.Log("No order-----------------------------------------------");
+				
 				float rotationOffest = (addRotation*clickCount)%360;
-				tween.from = gameObject.transform.rotation.eulerAngles;
-				tween.to = initialRotation + (new Vector3(0,0,rotationOffest));
-				tween.Reset();
-				tween.enabled = true;
+				//tween.from = gameObject.transform.rotation.eulerAngles;
+				//tween.to = initialRotation + (new Vector3(0,0,rotationOffest));
+				//tween.Reset();
+				//tween.enabled = true;
 				
 				//send data
 				//*** Have Bug
@@ -49,25 +62,34 @@ public class Puzzle2PanelSensor1 : MonoBehaviour {
 				
 			}
 		}
-
+		
 	}
 	void OnTriggerEnter(Collider other) {
 		isTrigger = true;
-
+		
 		//trigger the button B on the player
-		if(other.gameObject.tag == "Player")
+		if(other.gameObject.tag == "Player" && other.gameObject.networkView.isMine)
 			other.gameObject.GetComponent<ShowPressB>().showButtonB();
-
+		
 	}
 	
 	
 	void OnTriggerExit(Collider other) {
-
+		
 		isTrigger = false;
-
+		
 		//hide the button B on the player
-		if(other.gameObject.tag == "Player")
+		if(other.gameObject.tag == "Player" && other.gameObject.networkView.isMine)
 			other.gameObject.GetComponent<ShowPressB> ().hideButtonB ();
-
+		
+	}
+	void detectNumber(){
+		if (gameObject.name == "Puzzle 2 Sensor 1") {
+			order = 1;
+		} else if (gameObject.name == "Puzzle 2 Sensor 2") {
+			order = 2;
+		} else if (gameObject.name == "Puzzle 2 Sensor 3") {
+			order = 3;
+		}
 	}
 }
