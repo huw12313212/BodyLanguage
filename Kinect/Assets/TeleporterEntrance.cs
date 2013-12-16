a8838714962d51bcffc97ceaf9c32e6b2f43f06d
11sdfjsakl <hcy810125@yahoo.com.tw>
2013-12-16 21:56:30 +0800

    add "teleporter"~傳送門 again XD

---
+++ Kinect/Assets/TeleporterEntrance.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleporterEntrance : MonoBehaviour {
+
+	public ParticleSystem explosion;
+	public GameObject exit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		explosion.Play();
+
+		if (other.gameObject == CameraManager.CurrentPlayer1)
+		{
+			CameraManager.CurrentPlayer1.transform.position = exit.transform.position;
+
+		}
+	}
+}
---
+++ Kinect/Assets/TeleporterExit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleporterExit : MonoBehaviour {
+
+	public ParticleSystem explosion;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		explosion.Play();	
+	}
+}
