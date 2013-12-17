fbf32afa3653cd129649e4aab9f5c7d603c993cf
wang wei hang <wangweihang5566@gmail.com>
2013-12-17 22:24:50 +0800

    update scene

--- Kinect/Assets/PuzzleDoorScript.cs
+++ Kinect/Assets/PuzzleDoorScript.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class PuzzleDoorScript : MonoBehaviour {
 
 	public TweenPosition tween;
+	public Vector3 positionOffset;
 
 	public void open()
 	{
 		//open door
 		Debug.Log("Open the door");
 		tween.from = gameObject.transform.localPosition;
-		tween.to = gameObject.transform.localPosition + new Vector3(0,-10.0f,0);
+		tween.to = gameObject.transform.localPosition + positionOffset;
 		tween.Reset();
 		tween.enabled = true;
 	}
