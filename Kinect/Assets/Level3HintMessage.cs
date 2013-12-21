46a3ffcbcc961945360369b6811da4ad3e07e6f4
11sdfjsakl <hcy810125@yahoo.com.tw>
2013-12-21 21:14:53 +0800

    +level 3 UI

---
+++ Kinect/Assets/Level3HintMessage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level3HintMessage : MonoBehaviour {
+
+	//public 
+	public UILabel label;
+
+	void Awake()
+	{
+		GlobalSyncData sync = GameObject.FindGameObjectWithTag("GlobalSyncObject").GetComponent<GlobalSyncData>();
+		//labl.
+
+		label.text = sync.puzzle3Answer[0];
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
